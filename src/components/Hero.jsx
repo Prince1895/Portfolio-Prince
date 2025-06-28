c29577fde514b22368971da300eb063a0ea9e3e9
Prince1895 <chauhanprince21153366@gmail.com>
2025-06-28 14:02:56 +0530

    navbar completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import{motion} from 'framer-motion'
+import {avtar} from '../assets/avtarpng.png'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+  <>
+  <section className="w-full h-screen flex items-center justify-center px-4">
+      <div className="max-w-4xl w-full flex flex-col items-center text-center">
+  <motion.a
+  href="https://twitter.com/intent/follow?screen_name=Chauhan18Prince"
+  target="_blank"
+  rel="noopener noreferrer"
+  initial={{ opacity: 0, scale: 0.9 }}
+  animate={{ opacity: 1, scale: 1 }}
+  transition={{ duration: 0.6 }}
+>
+  <img
+    src={avtar}
+    alt="Follow on Twitter"
+    className="w-24 h-24 sm:w-28 sm:h-28 rounded-full border-2 border-black dark:border-white hover:scale-105 transition-transform duration-300 cursor-pointer"
+    title="Follow me on Twitter"
+  />
+</motion.a>
+</div>
+</section>
+  </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,8 @@ import { Link, useLocation } from 'react-router-dom';
 import { IoPerson } from 'react-icons/io5';
 import { RxGithubLogo, RxLinkedinLogo } from 'react-icons/rx';
 import { PiXLogo } from 'react-icons/pi';
+import { RiLinksLine } from 'react-icons/ri';
+import { Tooltip } from 'react-tooltip'
 
 const Navbar = () => {
   const location = useLocation();
@@ -17,33 +19,84 @@ const Navbar = () => {
 
           {/* Home */}
           <Link to="/">
+
             <motion.div whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.95 }}
-              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300">
-              <HomeIcon className={`w-[19px] h-[19px] text-black ${pathname === '/' ? 'text-[#cc9e2b]' : ''}`} />
+              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300"
+              data-tooltip-id="home-tooltip"
+              data-tooltip-content="Home">
+              <Tooltip id="home-tooltip"style={{
+      backgroundColor: '#000',
+      color: '#fff',
+      fontSize: '10px',
+      padding: '4px 8px',
+      borderRadius: '6px',
+      boxShadow: '0px 2px 8px rgba(0,0,0,0.2)',
+      zIndex: 9999,
+    }} />
+              <HomeIcon
+                className={`w-[19px] h-[19px] max-sm:w-[15px] max-sm:h-[15px] text-black dark:text-white ${pathname === "/" ? "dark:!text-[#FFC83D] !text-[#cc9e2b]" : ""}`}
+              />
+
             </motion.div>
+
           </Link>
+
 
           {/* Projects */}
           <Link to="/projects">
             <motion.div whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.95 }}
-              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300">
-              <Folders className={`w-[19px] h-[19px] text-black ${pathname === '/projects' ? 'text-[#cc9e2b]' : ''}`} />
+              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300"
+              data-tooltip-id="projects-tooltip"
+              data-tooltip-content="Projects">
+                <Tooltip id="projects-tooltip"style={{
+      backgroundColor: '#000',
+      color: '#fff',
+      fontSize: '10px',
+      padding: '4px 8px',
+      borderRadius: '6px',
+      boxShadow: '0px 2px 8px rgba(0,0,0,0.2)',
+      zIndex: 9999,
+    }} />
+              <Folders className={`w-[19px] h-[19px] max-sm:w-[15px] max-sm:h-[15px] text-black dark:text-white ${pathname === "/projects" ? "dark:!text-[#FFC83D] !text-[#cc9e2b]" : ""}`} />
             </motion.div>
           </Link>
 
           {/* About */}
           <Link to="/about">
             <motion.div whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.95 }}
-              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300">
-              <IoPerson className={`w-[19px] h-[19px] text-black ${pathname === '/about' ? 'text-[#cc9e2b]' : ''}`} />
+              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300"
+              data-tooltip-id="about-tooltip"
+              data-tooltip-content="About">
+                <Tooltip id="about-tooltip"style={{
+      backgroundColor: '#000',
+      color: '#fff',
+      fontSize: '10px',
+      padding: '4px 8px',
+      borderRadius: '6px',
+      boxShadow: '0px 2px 8px rgba(0,0,0,0.2)',
+      zIndex: 9999,
+    }} />
+              <IoPerson
+                className={`w-[19px] h-[19px] max-sm:w-[15px] max-sm:h-[15px] text-black dark:text-white ${pathname === "/about" ? "dark:!text-[#FFC83D] !text-[#cc9e2b]" : ""}`}
+              />
             </motion.div>
           </Link>
 
-       
+
           {/* Resume */}
-          <a href="https://drive.google.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://drive.google.com/file/d/16VAYIjaeAnD1UditSsEg-iK2q7RsY_06/view?usp=sharing" target="_blank" rel="noopener noreferrer">
             <motion.div whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.95 }}
-              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300">
+              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300" data-tooltip-id="resume-tooltip"
+              data-tooltip-content="Resume">
+                <Tooltip id="resume-tooltip"style={{
+      backgroundColor: '#000',
+      color: '#fff',
+      fontSize: '10px',
+      padding: '4px 8px',
+      borderRadius: '6px',
+      boxShadow: '0px 2px 8px rgba(0,0,0,0.2)',
+      zIndex: 9999,
+    }} />
               <FileIcon className="w-[19px] h-[19px] text-black" />
             </motion.div>
           </a>
@@ -51,7 +104,17 @@ const Navbar = () => {
           {/* GitHub */}
           <a href="https://github.com/Prince1895" target="_blank" rel="noopener noreferrer">
             <motion.div whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.95 }}
-              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300">
+              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300" data-tooltip-id="github-tooltip"
+              data-tooltip-content="Github">
+                <Tooltip id="github-tooltip"style={{
+      backgroundColor: '#000',
+      color: '#fff',
+      fontSize: '10px',
+      padding: '4px 8px',
+      borderRadius: '6px',
+      boxShadow: '0px 2px 8px rgba(0,0,0,0.2)',
+      zIndex: 9999,
+    }} />
               <RxGithubLogo className="w-[19px] h-[19px] text-black" />
             </motion.div>
           </a>
@@ -59,7 +122,17 @@ const Navbar = () => {
           {/* X (Twitter) */}
           <a href="https://x.com/Chauhan18Prince" target="_blank" rel="noopener noreferrer">
             <motion.div whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.95 }}
-              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300">
+              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300" data-tooltip-id="x-tooltip"
+              data-tooltip-content="X">
+                <Tooltip id="x-tooltip"style={{
+      backgroundColor: '#000',
+      color: '#fff',
+      fontSize: '10px',
+      padding: '4px 8px',
+      borderRadius: '6px',
+      boxShadow: '0px 2px 8px rgba(0,0,0,0.2)',
+      zIndex: 9999,
+    }} />
               <PiXLogo className="w-[19px] h-[19px] text-black" />
             </motion.div>
           </a>
@@ -67,8 +140,35 @@ const Navbar = () => {
           {/* LinkedIn */}
           <a href="https://linkedin.com/in/prince1184/" target="_blank" rel="noopener noreferrer">
             <motion.div whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.95 }}
-              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300">
+              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300" data-tooltip-id="linkedin-tooltip"
+              data-tooltip-content="LinkedIn">
+                <Tooltip id="linkedin-tooltip"style={{
+      backgroundColor: '#000',
+      color: '#fff',
+      fontSize: '10px',
+      padding: '4px 8px',
+      borderRadius: '6px',
+      boxShadow: '0px 2px 8px rgba(0,0,0,0.2)',
+      zIndex: 9999,
+    }} />
               <RxLinkedinLogo className="w-[19px] h-[19px] text-black" />
+            </motion.div>
+          </a>
+          {/* LinkTree */}
+          <a href="https://linktr.ee/prince1184" target="_blank" rel="noopener noreferrer">
+            <motion.div whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.95 }}
+              className="hover:px-3 py-2.5 hover:bg-[#F4F4F5] rounded-full transition-all duration-300" data-tooltip-id="linktree-tooltip"
+              data-tooltip-content="LinkTree">
+                <Tooltip id="linktree-tooltip"style={{
+      backgroundColor: '#000',
+      color: '#fff',
+      fontSize: '10px',
+      padding: '4px 8px',
+      borderRadius: '6px',
+      boxShadow: '0px 2px 8px rgba(0,0,0,0.2)',
+      zIndex: 9999,
+    }} />
+              <RiLinksLine className="w-[19px] h-[19px] text-black" />
             </motion.div>
           </a>
 
