2f191424808fb41dc04da31a84ff42fa000d5f27
Prince1895 <chauhanprince21153366@gmail.com>
2025-07-02 22:23:03 +0530

    change profile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import avtar from '../assets/avtarpng.png'
+import avtar from '../assets/profile_img.png'
 import { TypeAnimation } from 'react-type-animation'
 import { RainbowButton } from './magicui/rainbow-button'
 import { Link as ScrollLink } from 'react-scroll'
